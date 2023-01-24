9bd458f25a7e7e37edc85235896e65cf1ad7e218
Konny P Ho <conniebilac@gmail.com>
2023-01-23 22:15:51 -0500

    added code to sql files

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS management_system_db;
+CREATE DATABASE management_system_db;
+
+USE management_system_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE role (
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   title VARCHAR(50) NOT NULL,
+   salary DECIMAL,
+   department_id INT,
+   FOREIGN KEY (department_id)
+   REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role INT, 
+    FOREIGN KEY (role)
+    REFERENCES role(id)
+);
