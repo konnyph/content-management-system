b44576619d5b4554ceb349a6efa034b9d6282322
Konny P Ho <conniebilac@gmail.com>
2023-01-25 22:15:15 -0500

    Added prompt to index.js

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const inquirer = require("inquirer");
 require("console.table");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "management_system_db"
-  });
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "management_system_db"
+});
 
-  connection.connect(function (err) {
-    if (err) throw err;
-    // console.log(`data connected`);
-    firstPrompt();
-  });
+connection.connect(function (err) {
+  if (err) throw err;
+  // console.log(`data connected`);
 
+});
+firstPrompt();
 // Inquirer Prompt ///
 function firstPrompt() {
 
@@ -40,9 +40,9 @@ function firstPrompt() {
           viewEmployee();
           break;
         case "View All Departments":
-          viewEmployeeByDepartment();
+          viewDepartment();
           break;
-        
+
         case "Add Employee":
           addEmployee();
           break;
@@ -59,7 +59,7 @@ function firstPrompt() {
           addDepartment();
           break;
 
-        
+
         case "End":
           connection.end();
           break;
@@ -67,7 +67,36 @@ function firstPrompt() {
     });
 }
 
-// Add Employee()
+// View Employee()
 function viewEmployee() {
-  console.log("Viewing employees\n");
+  // console.log("Viewing employees\n");
+  connection.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id  LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+  )
+    .then(([data]) => {
+      console.table(data);
+    })
+    .then(() => {
+      firstPrompt();
+    });
+
 }
+// Add Department
+function addDepartment() {
+  inquirer.prompt({
+    type: "input",
+    name: "department",
+    message: "What is the department name?"
+  })
+    .then((answer) => {
+      connection.promise().query("INSERT INTO department SET ?", answer)
+      .then(() => {
+        console.log(`new department was added.`)
+        firstPrompt();
+      });
+
+    })
+    
+
+}
+
+
