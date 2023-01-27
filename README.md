fbd900a5a307c6a9df789641860edc6e54236cb6
Konny P Ho <conniebilac@gmail.com>
2023-01-26 21:40:42 -0500

    added more codes to index.js/added README file

---
+++ README.md
@@ -0,0 +1,36 @@
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+# Title: Content Management System
+
+## Description:
+an application runs by nodejs, inquirer, and mysql that stores and manage employee's information
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [test](#test)
+- [license](#license)
+- [question](#questions)
+
+## Installation: 
+N/A
+
+## Usage:
+the application is used to view employees, departments and role,  it can also be used to add or update employee
+
+## Contribution: 
+N/A 
+
+## Test:
+N/A
+## License: 
+[MIT License](LICENSE.txt)
+## Questions 
+conniebilac@gmail.com
+
+Github:
+konnyph
+## Walkthrough Video link:
+https://drive.google.com/file/d/19jNjVmPOoM85R4swFQGSyZnGHAC4EGpd/view
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "Bilac310.",
   database: "management_system_db"
 });
 
@@ -80,23 +80,127 @@ function viewEmployee() {
     });
 
 }
+// View Role
+function viewRole() {
+  connection.promise().query("SELECT  role.id, role.title,department.department, role.salary  FROM role INNER JOIN department on role.department_id = department.id")
+  .then(([data]) => {
+    console.table(data);
+  })
+  .then(() => {
+    firstPrompt();
+  });
+}
+// View all Department
+function viewDepartment() {
+  connection.promise().query("SELECT * FROM department;")
+  .then(([data]) => {
+    console.table(data);
+  })
+  .then( () => {
+    firstPrompt();
+  });
+}
 // Add Department
 function addDepartment() {
   inquirer.prompt({
     type: "input",
     name: "department",
-    message: "What is the department name?"
+    message: "What is the department name?",
+    
   })
-    .then((answer) => {
-      connection.promise().query("INSERT INTO department SET ?", answer)
-      .then(() => {
-        console.log(`new department was added.`)
-        firstPrompt();
+  .then((answer) => {
+    connection.promise().query("INSERT INTO department SET ?", answer)
+    .then(() => {
+      console.log(`new department was added.`)
+      firstPrompt();
       });
 
     })
-    
+}  
+//  Add Role:
 
+function addRole() {
+  var query =
+  `
+	SELECT  role.id, role.title,department.department, role.salary  FROM role 
+	INNER JOIN department on role.department_id = department.id`
+
+connection.query(query, function (err, res) {
+  if (err) throw err;
+  const departmentChoices = res.map(({ id, department}) => ({
+    value: id, name: `${id} ${department}`
+  }));
+
+  console.table(res);
+  console.log("Department array!");
+
+  promptAddRole(departmentChoices);
+});
+}
+
+function promptAddRole(departmentChoices) {
+   inquirer.prompt([
+  {
+    type: "input",
+    name: "title",
+    message: "What is the role title?",
+   },
+   {
+    type: "input",
+    name: "salary",
+    message: "What is the salary of the new role title?",
+   },
+   {
+    type: "list",
+    name: "department_id",
+    message:"Which department does the role belong to?",
+    
+    choices: departmentChoices
+   }
+  ]) 
+  .then(function (answer) {
+
+    var query = `INSERT INTO role SET ?`
+
+    connection.query(query, {
+      title: answer.title,
+      salary: answer.salary,
+      department_id: answer.departmentId
+    },
+      function (err, res) {
+        if (err) throw err;
+
+        console.table(res);
+        console.log("Role Inserted!");
+
+        firstPrompt();
+      });
+
+  });
+}
+// Add Employee
+function addEmployee() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "first_name",
+      message: "What is the employee's first name?"
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "What is the employee's last name?"
+    },
+    {
+      type: "list",
+      name: "role_id",
+      message:"What is the employee's role?",
+      choices: [`SELECT role.department_id FROM role`]
+      // choices: ["Sales Lead","Sales Person","Lead Engineer","Software Engineer","Account Manager","Accountant","Legal Team Lead","Lawyer",""]
+    },
+
+
+  ])
 }
 
 
